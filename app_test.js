3e6e0e8fbbcda0f26c4ac3ce2183c37658d6d144
Sowjanyamvs03 <112181088+Sowjanyamvs03@users.noreply.github.com>
2025-05-23 11:47:38 +0530

    test script added

---
+++ app_test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test', () => {
+  it('should pass this basic test', () => {
+    expect(1 + 1).to.equal(2);
+  });
+});
